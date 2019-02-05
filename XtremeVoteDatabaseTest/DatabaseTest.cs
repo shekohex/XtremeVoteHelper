2905acb9212828d26e2238c2e5f01a10187386ac
Shady Khalifa <shekohex@gmail.com>
2019-02-05 18:45:27 +0200

    Xtreme Vote Helper is now done

---
+++ XtremeVoteDatabaseTest/DatabaseTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XtremeVoteDatabaseTest
+{
+    [TestClass]
+    public class DatabaseTest
+    {
+        [TestMethod]
+        public void Connection()
+        {
+            var db = new XtremeVoteHelper.Database("votes.db");
+            Assert.IsNotNull(db);
+        }
+
+        [TestMethod]
+        public void CheckUserVotePoints()
+        {
+            var db = new XtremeVoteHelper.Database("votes.db");
+            var sehekohex_points = db.GetPlayerVotePoints("shekohex");
+            var unknown_points = db.GetPlayerVotePoints("unkwon");
+            Assert.AreEqual(sehekohex_points, 100);
+            Assert.AreEqual(unknown_points, 0);
+        }
+    }
+}
---
+++ XtremeVoteDatabaseTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XtremeVoteDatabaseTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XtremeVoteDatabaseTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9284f148-b22a-4822-851c-09a21403b5b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
